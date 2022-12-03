8682af82cc8dacc82ed038c2a723abab31443aac
Himali96 <himalikothariedu@gmail.com>
2022-12-03 17:26:40 -0500

    Lobby UI modified

---
+++ Assets/[Scripts]/Menu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
