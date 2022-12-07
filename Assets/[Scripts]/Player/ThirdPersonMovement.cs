eb6bd66562a112f6b80fb3874a0c3c4b1db32926
divyesh155 <d3v.p8l@gmail.com>
2022-12-06 22:59:07 -0500

    UI adjustments
    Camera changed to Top View
    Added Teleportation
    Changed Main Menu as first screen
    Added code for power up with power pallets

--- Assets/[Scripts]/Player/ThirdPersonMovement.cs
+++ Assets/[Scripts]/Player/ThirdPersonMovement.cs
@@ -7,23 +7,46 @@ public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;
     public Transform cam;
-    public float speed = 6f;
+    public float speed = 3.5f;
     public float turnSmoothTime = 0.1f;
     float turnSmoothvelocity;
     public bool HasPowerUp = false;
+    public GameObject _gameObject;
+    public float powerTime = 0.0f;
+
 
     [SerializeField]
     PhotonView pv;
 
   //  public bool HasPowerUp { get; internal set; }
 
+    public void Teleport(Vector3 position, Quaternion rotation)
+    {
+        transform.position = position;
+        Physics.SyncTransforms();
+        
+    }
+
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3 (horizontal, 0f, vertical).normalized;
+        _gameObject = GameObject.FindGameObjectWithTag("CherryImage");
 
-        if(direction.magnitude >= 0.1f)
+        powerTime -= Time.deltaTime;
+
+        if (powerTime > 0.0f && !HasPowerUp)
+        {
+            HasPowerUp = true;
+          
+        }
+        else if (HasPowerUp)
+        {
+            HasPowerUp = false;
+        }
+
+        if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothvelocity, turnSmoothTime);
@@ -42,12 +65,19 @@ public class ThirdPersonMovement : MonoBehaviour
         if(other.CompareTag("Cherry"))
         {
             PhotonNetwork.Destroy(other.gameObject);
-            HasPowerUp = true;
+            _gameObject.SetActive(true);
+            
         }
 
         if(other.CompareTag("Ghost") && HasPowerUp)
         {
             PhotonNetwork.Destroy(other.gameObject);
+        }
+
+        if (other.CompareTag("PowerUp"))
+        {
+            PhotonNetwork.Destroy(other.gameObject);
+            HasPowerUp = true;
         }
     }
 
