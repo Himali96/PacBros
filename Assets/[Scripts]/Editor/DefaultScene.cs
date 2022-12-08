1c7979650e62c821b0a24cc7e93c60675f4a2d1a
arkms <zerodel3000@hotmail.com>
2022-12-07 23:48:23 -0500

    Default scene

---
+++ Assets/[Scripts]/Editor/DefaultScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class DefaultScene : EditorWindow
+{
+    void OnGUI()
+    {
+        EditorSceneManager.playModeStartScene = (SceneAsset)EditorGUILayout.ObjectField(new GUIContent("Start Scene"), EditorSceneManager.playModeStartScene, typeof(SceneAsset), false);
+    }
+
+    [MenuItem("Tools/Default Scene")]
+    static void Open()
+    {
+        GetWindow<DefaultScene>();
+    }
+}
